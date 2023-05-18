36eacad99790eaa77557c7cf0b4326e94bc2a436
Sheikh Mohd Aamir <jok20001+work@gmail.com>
2023-05-18 19:15:34 +0530

    Added Summary

---
+++ src/Header/Summary.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Summary = () => {
+  return (
+    <div className="card text-bg-dark text-center mb-3" >
+        <h5 className="card-title">Delicious Food, Delivered To You</h5>
+        <p className="card-text">Choose your favourite meal from our broad selection of available meals and enjoy a elicious lunch or dinner at home.</p>
+        <p className="card-text">All our meals are cooked with high-quality ingredients, just-in-time and of course by experienced chefs!</p>
+    </div>
+  )
+}
+
+export default Summary
